524b828818fa6fa63d7f191a768639a86f904e4e
Krzysztof Rogowski <krzysztor99@gmail.com>
2024-01-04 14:19:12 +0100

    Fix braces

--- examples/pipe_closed.c
+++ examples/pipe_closed.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
     } else if (world_rank == 1) {
         test_assert(MIMPI_Recv(&number, 1, 0, tag) == MIMPI_ERROR_REMOTE_FINISHED);
         MIMPI_Retcode const ret = MIMPI_Send(&number, 1, 0, tag);
-        test_assert(ret == MIMPI_ERROR_REMOTE_FINISHED || ret == MIMPI_SUCCESS);
+        test_assert((ret == MIMPI_ERROR_REMOTE_FINISHED || ret == MIMPI_SUCCESS));
         printf("Process 1 received number %d from process 0\n", number);
     }
 
