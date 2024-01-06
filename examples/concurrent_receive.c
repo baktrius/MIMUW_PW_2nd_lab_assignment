b6e2108d30f17aad5009fe753e1a8fc68542e486
Wojciech Malecha <wm448525@students.mimuw.edu.pl>
2024-01-06 09:27:36 +0100

    sending with ANY_TAG is UB

--- examples/concurrent_receive.c
+++ examples/concurrent_receive.c
@@ -8,7 +8,7 @@ int main() {
     if (MIMPI_World_rank() != 0) {
         int a[10];
         for (int i = 0; i < 10000; i++) {
-            MIMPI_Send(a, 10 * sizeof(int), 0, MIMPI_ANY_TAG);
+            MIMPI_Send(a, 10 * sizeof(int), 0, 1);
         }
     }
 
