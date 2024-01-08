bf0834c681de3e1544935526e3d7d6ead3da52ec
Stanisław Czech <sc@lo3gdynia.pl>
2024-01-08 19:59:26 +0100

    Valgrind tests

--- tests/pangolingila/deadlock1.sh
+++ tests/pangolingila/deadlock1.sh
@@ -1,5 +1,10 @@
 set -e
-for i in {1..100} ; do
+if [ -z ${VALGRIND+x} ]; then
+    inter=100
+else
+    inter=10
+fi
+for i in {1..$inter} ; do
     ./run_test 1 2 examples_build/deadlock1
     ./run_test 1 2 examples_build/deadlock2
     ./run_test 1 2 examples_build/deadlock3
